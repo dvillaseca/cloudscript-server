56d43267b2c43b8b29ce224a3eff5f32d8e286fe
josetinybytes <jguerra@tinybytes.com>
2025-06-16 22:51:39 -0600

    create function for removing console.logs (just some tests for the future)

--- src/compilers/releaseCompiler.js
+++ src/compilers/releaseCompiler.js
@@ -2,6 +2,7 @@ const compilerUtils = require('./compilerUtils');
 const fs = require('fs');
 const path = require('path');
 const UglifyJS = require("uglify-js");
+const { Project, SyntaxKind, CallExpression } = require('ts-morph');
 
 
 
@@ -14,12 +15,32 @@ module.exports.compile = (location) => {
     let files = compilerUtils.getFiles(location);
 
     let outFile = "";
+    outFile += `const _=()=>{};\n`;
     for (let file of files) {
-        outFile += compilerUtils.processFile(file);
+        let content = compilerUtils.processFile(file);
+        // content = replaceConsoleLogs(content);
+        outFile += content;
         outFile += '\n';
     }
     let minified = UglifyJS.minify(outFile, { compress: true });
     if (minified.error)
         throw new Error(minified.error);
     return minified.code;
+}
+function replaceConsoleLogs(content, target = "_()") {
+    if (!content.includes("console.log("))
+        return content;
+
+    const project = new Project({ useInMemoryFileSystem: true });
+    const sourceFile = project.createSourceFile("temp.ts", content, { overwrite: true });
+
+    // SAFELY collect nodes before modifying
+    const calls = sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression)
+        .filter(call => call.getExpression().getText() === "console.log");
+
+    for (const call of calls) {
+        call.replaceWithText(target);
+    }
+
+    return sourceFile.getFullText();
 }
